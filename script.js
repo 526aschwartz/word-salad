6b7ffebbc43c862c26d6c73965ef83dbbd718c0d
526aschwartz <526aschwartz@frhsd.com>
2024-11-13 09:29:46 -0500

    finished story

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let userName = prompt('What is your name?')
-let userAge = prompt('How old are you?')
-console.log(userName)
-console.log(userAge)
+let spaceOne = prompt('Plural Noun')
+let spaceTwo = prompt('Food Or Drink')
+let spaceThree = prompt('Famous NYC Location')
+let spaceFour = prompt('Adjective')
+let spaceFive = prompt('Adjective')
+let spaceSix = prompt('Famous NYC Street')
+let spaceSeven = prompt('Adjective')
+let spaceEight = prompt('Noun')
+let spaceNine = prompt('Noun')
+let spaceTen = prompt('Plural Noun')
+let spaceEleven = prompt('Holiday Song')
+let spaceTwelve = prompt('Adjective')
+let spaceThirteen = prompt('Famous NYC Location')
+let spaceFourteen = prompt('Adjective')
 
-let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge} years old?`
+console.log(spaceOne)
+console.log(spaceTwo)
+console.log(spaceThree)
+console.log(spaceFour)
+console.log(spaceFive)
+console.log(spaceSix)
+console.log(spaceSeven)
+console.log(spaceEight)
+console.log(spaceNine)
+console.log(spaceTen)
+console.log(spaceEleven)
+console.log(spaceTwelve)
+console.log(spaceThirteen)
+console.log(spaceFourteen)
+
+let myStory = `Last Christmas, I traveled to New York to see the holiday lights. The streets were filled with ${spaceOne}, and I could smell ${spaceTwo} in the air. I walked through ${spaceThree} and was amazed by the giant Christmas tree, which was so ${spaceFour}. I stopped to buy a ${spaceFive} scarf from a street vendor and continued toward ${spaceSix}. Suddenly, a ${spaceSeven} pigeon flew by, nearly knocking over my ${spaceEight}! I laughed and headed into a cozy café for a cup of ${spaceNine}. There, I saw a group of people dressed as ${spaceTen}, singing ${spaceEleven}. It was so ${spaceTwelve} that I couldn’t help but join in. Afterward, I walked down to ${spaceThirteen} to see the lights. As I left, I felt so ${spaceFourteen} and promised to come back next year!
+`
+
 console.log(myStory)
 
 document.getElementById('Story').innerHTML = myStory;
